685e387f668fd62c88b193a26ee86e2b64fcce26
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-09-16 18:16:54 -0300

    Feat: Primer Commit

---
+++ react-native-studing-blank/components/GameCard.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useRef, useEffect } from 'react';
+import {
+  Animated,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export const GameCard = ({ game }) => {
+  return (
+    <View
+      key={game.slug}
+      style={styles.card}>
+      <Image
+        source={{ uri: game.image }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>{game.title}</Text>
+      <Text style={styles.score}>{game.score}</Text>
+      <Pressable
+        style={styles.details}
+        onPress={() => console.log('Pressed')}>
+        <Text style={styles.textDetails}>Details</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export function AnimatedGameCard({ game, index }) {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      delay: index * 500,
+      useNativeDriver: true,
+    }).start();
+  }, [opacity, index]);
+
+  return (
+    <Animated.View style={{ opacity }}>
+      <GameCard game={game} />
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  card: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  title: {
+    color: '#000',
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  score: { color: '#000', fontSize: 12, marginTop: 5, fontSize: 14 },
+  details: {
+    backgroundColor: '#2f2f2f',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  textDetails: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
---
+++ react-native-studing-blank/components/Main.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { View, FlatList, ActivityIndicator } from 'react-native';
+import { getLatestGames } from '../lib/metacritic';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { AnimatedGameCard } from './GameCard';
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then(data => {
+      setGames(data);
+    });
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      {games.length === 0 ? (
+        <ActivityIndicator
+          size="large"
+          color="#fff"
+        />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={game => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard
+              game={item}
+              index={index}
+            />
+          )}
+        />
+      )}
+    </View>
+  );
+}
