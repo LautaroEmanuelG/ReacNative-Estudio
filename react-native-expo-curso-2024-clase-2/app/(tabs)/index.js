73e1271d1796712096ee1c9154637c10284209d9
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-09-20 11:19:16 -0300

    Fix (Apps) : Arregladas y funcionando error de Android Studio

---
+++ react-native-expo-curso-2024-clase-2/app/(tabs)/index.js
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { Main } from "../../components/Main";
+
+export default function Index() {
+  return (
+    <>
+    <Text>Latest Games</Text>
+    <Main />
+    </>
+  )
+}
--- react-native-studing-blank/components/Main.jsx
+++ react-native-studing-blank/components/Main.jsx
@@ -1,35 +1,48 @@
 import { useEffect, useState } from 'react';
-import { View, FlatList, ActivityIndicator } from 'react-native';
+import { View, FlatList, ActivityIndicator, Text } from 'react-native';
 import { getLatestGames } from '../lib/metacritic';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { AnimatedGameCard } from './GameCard';
 
 export function Main() {
   const [games, setGames] = useState([]);
-  const insets = useSafeAreaInsets();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const insets = useSafeAreaInsets(); // Definir insets
 
   useEffect(() => {
-    getLatestGames().then(data => {
-      setGames(data);
-    });
+    const fetchGames = async () => {
+      try {
+        const latestGames = await getLatestGames();
+        setGames(latestGames);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchGames();
   }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (error) {
+    return <Text>Error: {error}</Text>;
+  }
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
       {games.length === 0 ? (
-        <ActivityIndicator
-          size="large"
-          color="#fff"
-        />
+        <ActivityIndicator size="large" color="#fff" />
       ) : (
         <FlatList
           data={games}
           keyExtractor={game => game.slug}
           renderItem={({ item, index }) => (
-            <AnimatedGameCard
-              game={item}
-              index={index}
-            />
+            <AnimatedGameCard game={item} index={index} />
           )}
         />
       )}
